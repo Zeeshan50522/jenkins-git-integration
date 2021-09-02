79ef3d5b028faa2324d8c5046106f60ec63cdfa4
muhammad zeeshan <zeeshanwaheed50522@gmail.com>
2021-09-03 03:51:44 +0500

    npm jenkins

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-console.log("hello world")
+const randomNumbers = require('random-numbers')
+ 
+console.log('random', randomNumbers.create())
+console.log('random', randomNumbers.create(1,10))
+console.log('random', randomNumbers.create(99, 36489))
